d36cc7fe2da0bdfd41cf6df9e65871dea1c29624
Rohan <rohan1c47@gmail.com>
2025-07-18 21:29:29 +0530

    main.py
    making it readable + making the query only for english tweets

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ bearer_token = os.getenv("BEARER_TOKEN")
 if not bearer_token:
     raise Exception("Bearer token not loaded. Check your .env file.")
 
-client = tweepy.Client(bearer_token=bearer_token)
-query = "football"
+client = tweepy.Client(bearer_token=bearer_token)   #getting the client using the bearer token
+query = "football -is:retweet lang:en"    #reducing unwanted tweets related the query by changing the lanuage to english
 
 while True:
     try:
         tweets = client.search_recent_tweets(query=query, max_results=10)
         for tweet in tweets.data:
             print(tweet.text)
-        break  # exit the loop if successful
+        break  
 
     except tweepy.TooManyRequests as e:
         reset_timestamp = int(e.response.headers.get("x-rate-limit-reset", 0))
         reset_time = datetime.datetime.fromtimestamp(reset_timestamp)
         now = datetime.datetime.now()
-        wait_time = (reset_time - now).total_seconds()
+        wait_time = (reset_time - now).total_seconds()     #calculating the exact waiting period
 
-        print(f"\n❌ Rate limit hit. Waiting until {reset_time.strftime('%H:%M:%S')} ({int(wait_time)} seconds)")
-        time.sleep(max(wait_time, 0) + 1)  # wait and retry
+        print(f"\nRate limit hit. Waiting until {reset_time.strftime('%H:%M:%S')} ({int(wait_time)} seconds)")
+        time.sleep(max(wait_time, 0) + 1)  
 
     except Exception as e:
         print("Something else went wrong:", e)
